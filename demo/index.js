c6f307060c53917a2f2ca88821a7e8f4ef77ff65
Vu Ngoc An <anvn@vgroup.vn>
2017-12-05 02:32:34 +0700

    Forks

--- demo/index.js
+++ demo/index.js
@@ -34,30 +34,7 @@ class Demo extends React.Component {
         <Slider.Item style={{ color: '#333' }}>
           <div className="content love">
             <i className="fa fa-heart" />
-            <iframe
-              src="http://ghbtns.com/github-btn.html?user=daviferreira&repo=react-viewport-slider&type=follow&count=true&size=large"
-              allowTransparency="true"
-              frameBorder="0"
-              scrolling="0"
-              width="auto"
-              height="30"
-            />
-            <iframe
-              src="http://ghbtns.com/github-btn.html?user=daviferreira&repo=react-viewport-slider&type=watch&count=true&size=large"
-              allowTransparency="true"
-              frameBorder="0"
-              scrolling="0"
-              width="auto"
-              height="30"
-            />
-            <iframe
-              src="http://ghbtns.com/github-btn.html?user=daviferreira&repo=react-viewport-slider&type=fork&count=true&size=large"
-              allowTransparency="true"
-              frameBorder="0"
-              scrolling="0"
-              width="auto"
-              height="30"
-            />
+
           </div>
         </Slider.Item>
       </Slider>
--- src/Slider.js
+++ src/Slider.js
@@ -1,103 +1,153 @@
-'use strict';
+'use strict'
 
-import PropTypes from 'prop-types';
+import PropTypes from 'prop-types'
 
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
-import Item from './Item';
-import Paginator from './Paginator';
-import scrollToY from 'scroll-to-y';
+import Item from './Item'
+import Paginator from './Paginator'
+import scrollToY from 'scroll-to-y'
+import WheelReact from 'wheel-react'
 
 export default class Slider extends Component {
-  constructor(props) {
-    super(props);
+  constructor (props) {
+    super(props)
 
-    this.state = { activeIndex: 1 };
+    this.state = {
+      refLength: 0,
+      activeIndex: 1
+    }
 
-    this.setActive = this.setActive.bind(this);
-    this.handleScroll = this.handleScroll.bind(this);
-    this.lastScroll = 0;
+    this.setActive = this.setActive.bind(this)
+    this.scrollUp = this.scrollUp.bind(this)
+    this.scrollDown = this.scrollDown.bind(this)
+    // this.handleScroll = this.handleScroll.bind(this)
+    //this.lastScroll = 0
 
+    WheelReact.config({
+      left: () => {
+        this.setState({
+          content: 'left direction detected.'
+        })
+      },
+      right: () => {
+        this.setState({
+          content: 'right direction detected.'
+        })
+      },
+      up: () => {
+        // this.setState({
+        //   content: 'up direction detected.'
+        // })
+        console.log('upppppp')
+        this.scrollUp()
+      },
+      down: () => {
+        console.log('downnnnnn')
+        this.scrollDown()
+        // this.setState({
+        //   content: 'down direction detected.'
+        // })
+      }
+    })
     window.addEventListener('scroll', this.handleScroll);
   }
 
-  componentWillUnmount() {
-    window.removeEventListener('scroll', this.handleScroll);
+  scrollUp(){
+    console.log('upppppp111111')
+    console.log(this.state.activeIndex)
+    if(this.state.activeIndex !== 1){
+      const activeIndex = this.state.activeIndex - 1
+      this.setActive(activeIndex, true)
+      this.setState({activeIndex: activeIndex})
+    }
   }
 
-  handleScroll() {
-    if (this.isAnimating) {
-      return;
+  scrollDown(){
+    console.log('downnnnnn11111')
+    console.log(this.state.activeIndex)
+    console.log(this.refs)
+    if(this.state.activeIndex !== Object.keys(this.refs).length) {
+      const activeIndex = this.state.activeIndex + 1
+      this.setActive(activeIndex, true)
+      this.setState({activeIndex: activeIndex})
     }
-
-    const { activeIndex } = this.state;
-
-    // up
-    if (
-      window.scrollY > this.lastScroll &&
-      window.innerHeight + window.scrollY >
-        window.innerHeight * activeIndex + window.innerHeight / 2
-    ) {
-      this.setActive(activeIndex + 1);
-      // down
-    } else if (
-      window.scrollY < this.lastScroll &&
-      window.innerHeight + window.scrollY <
-        window.innerHeight * activeIndex - window.innerHeight / 1.5
-    ) {
-      this.setActive(activeIndex - 1);
-    }
-
-    this.lastScroll = window.scrollY;
   }
 
-  setActive(index, scrollTo) {
-    this.setState({ activeIndex: index }, () => {
+// componentWillUnmount() {
+//   window.removeEventListener('scroll', this.handleScroll);
+// }
+
+// handleScroll() {
+//   if (this.isAnimating) {
+//     return;
+//   }
+//   const { activeIndex } = this.state;
+//   // up
+//   if (
+//     window.scrollY > this.lastScroll &&
+//     window.innerHeight + window.scrollY >
+//       window.innerHeight * activeIndex + window.innerHeight / 2
+//   ) {
+//     this.setActive(activeIndex + 1);
+//     // down
+//   } else if (
+//     window.scrollY < this.lastScroll &&
+//     window.innerHeight + window.scrollY <
+//       window.innerHeight * activeIndex - window.innerHeight / 1.5
+//   ) {
+//     this.setActive(activeIndex - 1);
+//   }
+//
+//   this.lastScroll = window.scrollY;
+// }
+
+  setActive (index, scrollTo) {
+    this.setState({activeIndex: index}, () => {
       if (scrollTo) {
-        this.isAnimating = true;
+        this.isAnimating = true
         scrollToY(
           this.refs[`slide-${index}`].offsetTop,
           500,
           'easeInOutQuint',
           () => {
-            this.isAnimating = false;
+            this.isAnimating = false
           }
-        );
+        )
       }
-    });
+    })
   }
 
-  render() {
-    const { children } = this.props;
-    const { activeIndex } = this.state;
+  render () {
+    const {children} = this.props
+    const {activeIndex} = this.state
 
     if (!children) {
-      return null;
+      return null
     }
 
     return (
-      <div className="viewport-slider">
+      <section {...WheelReact.events} className="viewport-slider">
         <Paginator
-          activeIndex={activeIndex}
-          bullets={children.length}
-          onClick={this.setActive}
+          activeIndex={ activeIndex }
+          bullets={ children.length }
+          onClick={ this.setActive }
         />
 
-        {children.map((child, key) => {
-          let index = key + 1;
-
+        { children.map((child, key) => {
+          let index = key + 1
           return (
-            <div ref={`slide-${index}`} key={index}>
-              {React.cloneElement(child, {
+            <div ref={ `slide-${index}` } key={ index }>
+              { React.cloneElement(child, {
                 index,
                 hideButton: index === children.length,
                 onClick: this.setActive
-              })}
+              }) }
             </div>
-          );
-        })}
-      </div>
-    );
+          )
+        }) }
+      </section>
+    )
   }
 }
 
@@ -106,6 +156,6 @@ Slider.propTypes = {
     PropTypes.arrayOf(PropTypes.node),
     PropTypes.node
   ])
-};
+}
 
-Slider.Item = Item;
+Slider.Item = Item
